d706d814dbde79ab997778a492ebfdc8d2eec2a8
lemonyatko <dahnyuk2101s@gmail.com>
2023-01-03 18:07:12 +0200

    changed http method

--- src/listings/LinstingRouter.ts
+++ src/listings/LinstingRouter.ts
@@ -4,7 +4,7 @@ import ListingController from "./ListingController";
 const listingRouter = Router();
 
 listingRouter.post('/listing', ListingController.createListing);
-listingRouter.post('/listing/update', ListingController.updateListing);
+listingRouter.put('/listing', ListingController.updateListing);
 listingRouter.get('/listing', ListingController.getListings);
 listingRouter.delete('/listing', ListingController.deleteListing);
 
