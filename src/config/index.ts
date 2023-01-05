444c33992159a1d39f5217148cdac1c8b853b17e
lemonyatko <dahnyuk2101s@gmail.com>
2023-01-05 10:41:11 +0200

    new fields for mailService

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,9 @@ export const config = {
     BOT_TOKEN: "",
     BOT_NAME: "",
     JWT_REFRESH_SECRET: "secret-refresh",
-    JWT_ACCESS_SECRET: "secret-access"
+    JWT_ACCESS_SECRET: "secret-access",
+    SMTP_HOST: "",
+    SMTP_PORT: 0,
+    SMTP_USER: "",
+    SMTP_PASSWORD: ""
 }
